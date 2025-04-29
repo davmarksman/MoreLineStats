a0d3f00418430cc49881c1050f83547f7f42f836
davmarksman <david@brokit.co.uk>
2025-04-29 14:40:50 +0100

    Release 2025_04_29

--- mod.lua
+++ mod.lua
@@ -22,19 +22,17 @@ LINES
 This shows show additonal stats about lines. Only useful for passenger lines:
 - Average passenger waiting time
 - How many passengers are waiting at a stop
-- How many passengers have been waiting for longer than the line frequency (plus 1 minute in case of delays)
+- How many passengers have been waiting for longer than the line frequency (plus 1 minute in case of delays). Aka there was not enough space on the last vehicle for them
 - Passengers travelling on the line vs waiting
 - Leg times between stops
 - Competing lines (speed is the biggest deciding factor for which lines passengers pick. This allows you to see if a line is faster and therefore getting a higher share of passengers. I've noticed that the platform chosen on larger stations can have a noticable impact.)
 - Vehicles on line
 
 LOST TRAINS
-This provides a list of potentially lost trains. Trains are thought to be lost when they've been travelling for longer than 2x the maximum leg time or 3x the average leg time between stations
+This provides a list of potentially lost trains. Trains are thought to be lost when they've been travelling for longer than 1.5x the maximum leg time or 3x the average leg time between stations
 
 OTHER
 - This mod doesn't show any stats about cargo
-- Future plans include showing the rate of change at stops which will provide a line rate vs demand estimate
-- Let me know in the comments if you encounter any issues or would like to add any feature requests. 
 
 CREDITS
 - The ui and some helper functions are based off Celmi's Timetables mod
@@ -44,13 +42,19 @@ CREDITS
 REPO
 https://github.com/davmarksman/MoreLineStats
 
+FUTURE PLANS
+- Icons for each transport mode. Thanks to omegamezle/Mezzie
+- Future plans include showing the rate of change at stops which will provide a line rate vs demand estimate
+- Let me know in the comments if you encounter any issues or would like to add any feature requests. 
+
 ---
 
 UPDATE 1.1
-- Icons for each transport mode. Thanks to omegamezle/Mezzie
-- Change from How many passengers arrived within the last 5 minutes to passengers waiting longer than Line frequency
+- Change from "How many passengers arrived within the last 5 minutes" to "Passengers waiting longer than Line frequency"
 - Added a list of Vehicles on the line
-- Some QOL and usability fixes
+- QOL, UX and usability fixes
+ 
+
 ]]),
 		},
 	}
